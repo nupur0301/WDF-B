95bdb960919eafaefeb118345eb776b441154b0b
nupur0301 <nupurpadaria58@gmail.com>
2026-09-17 15:30:55 +0530

    Add files via upload

---
+++ Practical-04/faq.js
@@ -0,0 +1,18 @@
+const questions = document.querySelectorAll(".faq-question");
+
+questions.forEach(function(question){
+
+    question.addEventListener("click", function(){
+
+        const answer = this.nextElementSibling;
+
+        if(answer.style.display === "block"){
+            answer.style.display = "none";
+        }
+        else{
+            answer.style.display = "block";
+        }
+
+    });
+
+});
