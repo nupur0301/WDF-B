d986eb5120e153fc6adfd69cfcc16b41fb8abff1
nupur0301 <nupurpadaria58@gmail.com>
2026-09-17 15:26:05 +0530

    Delete Practical-04 directory

--- Practical-04/notification.js
+++
@@ -1,7 +0,0 @@
-const closeButton = document.getElementById("closeNotification");
-
-if(closeButton){
-    closeButton.addEventListener("click", function () {
-        document.getElementById("notificationBanner").style.display = "none";
-    });
-}
